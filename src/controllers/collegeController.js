eb2753f400ad6872ffba574d6a51a7144fba2d10
Md-Modassar <mdmodassar622@gmail.com>
2022-11-22 12:33:06 +0530

    all api complite

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,13 +1,13 @@
 const collegeModels=require("../Models/collegeModel")
 const interModels=require("../Models/internModel")
-
+const {isValid}=require("../Middlewares/InternValidator")
 
 
 
 const createAuthor=async (req,res)=>{
     try{
     const data=req.body
-    const createdData=await collegeModel.create(data)
+    const createdData=await collegeModels.create(data)
     res.status(201).send({status:true,message:createdData})
 }
 catch(error){
@@ -16,35 +16,27 @@ catch(error){
 }
 
 
-
-
-
-
 const getcollegedetails=async function(req,res){
     const collegename=req.query.collegename
-    if(!collegename)
-    {return res.status(400).send({msg:"please enter collegename in query params"})
-    }
-    if(collegename=="")  {
-            return res.status(400).send({status:false, msg:"please enter college in correct formment"})
-        }
-        if(!collegename==toLowerCase())
+
+       if(!isValid(collegename))return res.status(400).send({status:false,msg:"please enter valide college name"})
+          const els=collegename.toLowerCase()
+       if(!collegename==els)
         {
-            return res.status(400).send({msg:"it is not valid"})
+            return res.status(400).send({status:false,satmsg:"it is not valid"})
         }
     
  const collegeid=await collegeModels.find({name:collegename})
- //const {name,fullname,logolink}=collegeid[0]
- //console.log(collegeid[0].fullName)
+
  if(!collegeid){
     res.status(400).send({status:false,msg:"this is not valide college name"})
  } 
  const interns=await interModels.find({collegeId:collegeid[0]._id}).select({collegeid:0})
-//console.log(interns)
+
  if(!interns){
     return res.status(400).send({status:false, msg:"not found data"})
  }
- //console.log({d:"+++++++++++",msg:collegeid.name})
+ 
  let data={
     name:collegeid[0].name,
     fullname:collegeid[0].fullName,
@@ -55,12 +47,7 @@ const getcollegedetails=async function(req,res){
 
  return res.status(200).send({data})
  
-//  const getnewdata=await collegeModels.find({_id:collegeid._id})//{$set:{interns:recodeofinstershipdetails}},{upsert:true},{new:true})
-//   console.log(getnewdata)
-//  if(getnewdata.length==0){
-//     return res.status(400).send({status:false,msg:"data not found"})
-//  }
-//  return res.status(201).send({status:true,msg:getnewdata})
+
  
 }
 
